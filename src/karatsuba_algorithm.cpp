30b5a878c17b1e93de6f6b67f3867fe662998700
dmitryivanov199 <dmitryivanov199@mail.ru>
2022-09-02 23:14:58 +0300

    Reject uint8_t dynamic arrays
    All processing will be made in std::string view

--- src/karatsuba_algorithm.cpp
+++ src/karatsuba_algorithm.cpp
@@ -1,42 +1,24 @@
+#include <iostream>
+
 #include "karatsuba_algorithm.h"
 
 static size_t get_n(const std::string &number);
 
-static void fetch_digits(const std::string &number, uint8_t *digits, size_t n);
-
-static void get_half(uint8_t *digits, uint8_t *half_digits, size_t n, half half);
+static std::string get_half(const std::string &number, size_t size, half half);
 
 std::string multiplie_karatsuba(const std::string &x, const std::string &y) {
     size_t n{get_n(x)};
-    uint8_t *x_digits = new uint8_t[n];
-    uint8_t *y_digits = new uint8_t[n];
-    fetch_digits(x, x_digits, n);
-    fetch_digits(y, y_digits, n);
     std::string result{""};
 
     if (n == 1) {
-        uint8_t result_digits = x_digits[0] * y_digits[0];
-        result = std::to_string(result_digits);
+        result = std::to_string(static_cast<uint8_t>(x[0] - '0') * static_cast<uint8_t>(y[0] - '0'));
     }
     else {
-        uint8_t *a = new uint8_t[n / 2];
-        uint8_t *b = new uint8_t[n / 2];
-        uint8_t *c = new uint8_t[n / 2];
-        uint8_t *d = new uint8_t[n / 2];
-
-        get_half(x_digits, a, n, half::LEFT);
-        get_half(x_digits, b, n, half::RIGHT);
-        get_half(y_digits, c, n, half::LEFT);
-        get_half(y_digits, d, n, half::RIGHT);
-
-        delete [] a;
-        delete [] b;
-        delete [] c;
-        delete [] d;
+        std::string a{get_half(x, n, half::LEFT)};
+        std::string b{get_half(x, n, half::RIGHT)};
+        std::string c{get_half(y, n, half::LEFT)};
+        std::string d{get_half(y, n, half::RIGHT)};
     }
-
-    delete [] x_digits;
-    delete [] y_digits;
 
     return result;
 }
@@ -45,26 +27,6 @@ size_t get_n(const std::string &number) {
     return number.size();
 }
 
-static void fetch_digits(const std::string &number, uint8_t *digits, size_t n) {
-    for (size_t i{0}; i < n; i++) {
-        digits[i] = number.at(i) - '0';
-    }
-}
-
-static void get_half(uint8_t *digits, uint8_t *half_digits, size_t n, half half) {
-    size_t i;
-
-    if (half == half::LEFT) {
-        i = 0;
-    }
-    else {
-        i = n / 2 - 1;
-    }
-
-    size_t j{0};
-
-    for (; i < n; i++) {
-        half_digits[j] = digits[i];
-        j++;
-    }
+static std::string get_half(const std::string &number, size_t size, half half) {
+    return (half == half::LEFT)? number.substr(0, size / 2) : number.substr(size / 2, size / 2);
 }
